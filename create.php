df64adcb67a7095b5fc2afda7ea064ff1ba21b66
Ryosuke ONOSE <mail@onose.ws>
2016-08-06 23:25:23 +0900

    fix webres

--- create.php
+++ create.php
@@ -1,15 +1,16 @@
 <?php /* Template Name: new */
 $sid_num = 8;
 
-
 $sid = $_GET['sid'];
 $venue = urlencode($_GET['venue']);
 
 if(mb_strlen($sid) != $sid_num){
   http_response_code(400);
+  exit;
 }
 if(mb_strlen($venue) < 1){
   http_response_code(400);
+  exit;
 }
 
 http_response_code(201);
