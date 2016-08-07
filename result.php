bb87d6a974592a3a12e5ed40889388b620146ede
Ryosuke ONOSE <mail@onose.ws>
2016-08-07 14:43:33 +0900

    スタイルを適用

--- result.php
+++ result.php
@@ -1,13 +1,14 @@
 <?php /* Template Name: result*/
+get_header();
 
 $sid = $_GET['sid'];
 $venue = $_GET['venue'];
 
 $term = get_term_by('name', $venue, 'venue');
 
-echo $venue . " の " . 
+echo "<h1 id='shugo'>". $venue . " の " . 
   "<span id='venue-spot'></span>" . 
-  "に集合";
+  "に集合します。</h1>";
 
 
 // wp_update_term($term->term_id, 'venue', array(
@@ -26,20 +27,32 @@ loadTickets();
 function loadTickets(){
     if(timerStopFlag) return;
     $.ajax({
-        url: '<?php echo home_url('/') . "result_json/?venue=$venue"; ?>',
-        dataType: 'json',
-        success: function(data){
-          if(data == null){
-            $('#close-btn').val('募集を締め切りました。').attr('disabled', 'disabled');
-            return;
-          }
-            console.log(data);
-            $('.nameData > *').remove();
-            $.each(data, function(i){
-                $('.nameData').append("<li>" + data[i].post_author + "</li>")
-            });
-            setTimeout(loadTickets, 5000);
+      url: '<?php echo home_url('/') . "result_json/?venue=$venue"; ?>',
+      dataType: 'json',
+      success: function(data){
+        if(data == null){
+          $('#close-btn').val('募集を締め切りました。').attr('disabled', 'disabled');
+          return;
         }
+        console.log(data);
+        $('.nameData > *').remove();
+        $.each(data, function(i){
+          var realtime = new Date();
+          var dt1 = new Date(realtime.getFullYear(),realtime.getMonth(),realtime.getDate(),realtime.getHours(),realtime.getMinutes(),realtime.getSeconds());
+          console.log("現在時刻dt1=",dt1);
+          var dt2 = new Date(data[i].post_date);
+          console.log("post_date=",dt2);
+          var diff = Math.floor((dt1-dt2)/1000); //差を秒で取得
+
+          if (diff>60) {//60秒より多い場合
+              $('.nameData').prepend("<li><span class='sid'>" + data[i].post_author +"</span><span class='dates'>"+ Math.floor(diff/60) + "分前の投稿</span></li>");
+          }else {
+              $('.nameData').prepend("<li><span class='sid'>" + data[i].post_author +"</span><span class='dates'>"+ diff + "秒前の投稿</span></li>");
+          }
+          // $('.nameData').append("<li>" + data[i].post_author + "</li>")
+        });
+        setTimeout(loadTickets, 5000);
+      }
     });
     $.ajax({
         url: '<?php echo home_url('/') . "get_venue_description/?venue=$venue"; ?>',
@@ -72,3 +85,4 @@ $(document).ready(function(){
   }).css('cursor', 'pointer');
 });
 </script>
+<?php get_footer(); ?>
