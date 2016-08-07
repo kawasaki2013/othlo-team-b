605eac1fcd374fe8dad2e6feeb5e01eb6e5d9f02
Ryosuke ONOSE <mail@onose.ws>
2016-08-07 15:47:35 +0900

    header.php を編集

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<title>みんなめし</title>
 <link media="screen" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
+<meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 <body>
